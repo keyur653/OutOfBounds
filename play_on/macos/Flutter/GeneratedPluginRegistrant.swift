ece5357bbef36e7fe5b134a3b81b64007e7a9d4f
Keyur <keyurdoshi2270@gmail.com>
2023-01-31 21:07:47 +0530

    ui changed

--- play_on/macos/Flutter/GeneratedPluginRegistrant.swift
+++ play_on/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,19 +8,13 @@ import Foundation
 import cloud_firestore
 import firebase_auth
 import firebase_core
-import firebase_storage
 import modal_progress_hud_nsn
-import path_provider_macos
 import shared_preferences_foundation
-import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-  FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   ModalProgressHudNsnPlugin.register(with: registry.registrar(forPlugin: "ModalProgressHudNsnPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
